1041a0cba96a719c5a248f4f4f48682155870ac7
Will Shapiro <willshapiro@will-2.local>
2021-02-23 02:49:20 -0500

    changed sim to video

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import { init } from "ityped";
 import Graph from './Graph';
 import Projects from './Projects'
-
+import backgroundVideo from "./video/caSimulationCroppedShort.mp4"
 export default class Greeting extends Component {
   componentDidMount() {
     const myElement = document.querySelector("#myElement");
@@ -15,7 +15,15 @@ export default class Greeting extends Component {
       <>
       {/* <NavigationContainer> */}
     <div className="main-container">
-    <Graph />
+    {/* <Graph /> */}
+    
+    <video autoPlay loop muted id="myVideo">
+  <source src={backgroundVideo} type="video/mp4"/> 
+  Your browser does not support the video tag.
+
+  </video>
+  <div className="fading-container"></div>
+    
     <div id="welcome-container">
       <h1 className="welcome-statement">Hi, I'm Will!</h1>
       <h3 className="welcome-bio">Engineer, Developer, Entrepreneur</h3>
@@ -23,6 +31,8 @@ export default class Greeting extends Component {
     </div>    </div>
     
     {/* <Screener /> */}
+    
+    
     <Projects />
     {/* <Example /> */}
     {/* </NavigationContainer> */}
